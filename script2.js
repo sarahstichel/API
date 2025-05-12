afe90953887d413287498b63c346ec8c5e8e9bb4
Sarah <sarah.stichel@skola.taby.se>
2025-05-12 10:37:18 +0200

    klart

--- script2.js
+++ script2.js
@@ -3,17 +3,13 @@ const app = express();
 const mysql = require("mysql2/promise");
 const jwt = require("jsonwebtoken");
 
-// parse application/json, för att hantera att man POSTar med JSON
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt"); // installeras med npm install bcrypt
+const bcrypt = require("bcrypt");
 
-// Anropas med GET /gen-hash?password=kalleanka
-// Inställningar av servern.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 async function getDBConnnection() {
-  // Här skapas ett databaskopplings-objekt med inställningar för att ansluta till servern och databasen.
   return await mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -21,13 +17,6 @@ async function getDBConnnection() {
     database: "anvandare",
   });
 }
-
-//Gör något bra med decoded.
-
-app.get("/", (req, res) => {
-  res.send(`<h1>Doumentation EXEMPEL</h1>
-  <ul><li> GET /users</li><li>GET /name</li><li>GET /food/:foodType</li><li>GET /users/:username</li><li>POST /login - tar in JSON på formatet {"username":"", "password":""</li></ul>`);
-});
 
 function checkToken(req, res) {
   let authHeader = req.headers["authorization"];
@@ -44,36 +33,36 @@ function checkToken(req, res) {
   }
 }
 
+app.get("/", (req, res) => {
+  res.send(`<h1>Doumentation EXEMPEL</h1>
+  <ul><li> GET /users: Listar alla users</li><li>GET /users/:id: Listar usern med det angivna id:t; kräver giltig token</li><li>POST /users: skapar en ny user; kräver giltig token</li><li>PUT /users: uppdaterar namn och lösenord för en user; kräver giltig token</li><li>POST /login - checkar användarnamn och lösenord</li></ul>`);
+});
+
 app.get("/users", async (req, res) => {
   let connection = await getDBConnnection();
-  let sql = "SELECT * FROM users";
-
   decoded = checkToken(req, res);
   if (decoded) {
-    if (req.query.username) {
-      sql = `SELECT * FROM users WHERE username = ?`;
-    }
+    let sql = "SELECT * FROM users";
+
     let [results] = await connection.execute(sql);
-    res.json(results[0]);
+    let users = results;
+    users.forEach((user) => {
+      delete user.password;
+    });
+    res.json(users);
   }
 });
 
-app.get("/users/:username", async (req, res) => {
+app.get("/users/:id", async (req, res) => {
   decoded = checkToken(req, res);
   if (decoded) {
     let connection = await getDBConnnection();
-    let sql = `SELECT * FROM users WHERE username = ?`;
-    let [results] = await connection.execute(sql, [req.params.username]);
+    let sql = `SELECT * FROM users WHERE id = ?`;
+    let [results] = await connection.execute(sql, [req.params.id]);
     res.json(results);
+  } else {
+    res.status(401).send("Invalid auth token");
   }
-});
-
-app.get("/name", (req, res) => {
-  res.send(req.query);
-});
-
-app.get("/food/:foodType", (req, res) => {
-  res.send(req.params);
 });
 
 app.post("/users", async function (req, res) {
@@ -97,12 +86,14 @@ app.post("/users", async function (req, res) {
         req.body.name,
         hashedPassword,
       ]);
-      user.password = hashedPassword;
       user.id = results.insertId;
+      delete user.password;
       res.json(user);
     } else {
       res.sendStatus(422);
     }
+  } else {
+    res.status(401).send("Invalid auth token");
   }
 });
 
@@ -129,8 +120,11 @@ app.put("/users/:id", async (req, res) => {
     } else {
       res.sendStatus(500);
     }
+  } else {
+    res.status(401).send("Invalid auth token");
   }
 });
+
 app.post("/login", async (req, res) => {
   let connection = await getDBConnnection();
   let sql = `SELECT * FROM users WHERE username = ?`;
@@ -145,17 +139,18 @@ app.post("/login", async (req, res) => {
       let payload = {
         sub: user.id,
         name: user.name,
-        exp: Date.now() / 1000 + 60 * 120,
+        expiresIn: "2h",
       };
       let token = jwt.sign(payload, "ketchup");
-      res.json(token);
+      res.json(token).status(200);
     } else {
-      res.status(400).send("Felaktigt användarnamn eller lösenord");
+      res.status(401).send("Felaktigt användarnamn eller lösenord");
     }
   } else {
-    res.status(400).send("Felaktigt användarnamn eller lösenord");
+    res.status(401).send("Felaktigt användarnamn eller lösenord");
   }
 });
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
